dc0122c66321e184ead7f6e4e128786d8c8284c3
Woojae, Park <124398+nikescar@users.noreply.github.com>
2025-06-15 03:15:56 +0000

    fix: fix typo

--- plugins/available/os-repositories.plugin.bash
+++ plugins/available/os-repositories.plugin.bash
@@ -86,7 +86,7 @@ function __os-repository_check { # running_status 0 installed, running_status 5 
 function __os-repository_run {
     mkdir -p ${DURE_DEPLOY_PATH}/imgs/debian
     [[ $(mount |grep "${DURE_DEPLOY_PATH}/imgs/debian/"|wc -l) -lt 1 ]] && \
-        mount -o loop ${DURE_DEPLOY_PATH}/imgs/${DIST_PKG_IMG} ${DURE_DEPLOY_PATH}/imgs/debian
+        mount -o loop ${DURE_DEPLOY_PATH}/${DIST_PKG_IMG} ${DURE_DEPLOY_PATH}/imgs/debian
     apt install --fix-broken
 	return 0
 }
